1e2179ff77bca848e915ddf062ba9dcc1451fe2b
ensardz <enriquesaalas@gmail.com>
2018-01-03 22:44:09 -0600

    Some changes

---
+++ services/BudgetManagerAPI/app/models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose'),
+bcrypt = require('bcrypt');
+
+const Schema = mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    clients: [{}]
+});
+
+Schema.pre('save', function (next) {
+    const user = this;
+
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, (error, salt) => {
+            if (error) return next (error);
+
+            bcrypt.hash(user.password, salt, (error, hash) => {
+                if (error) return next (error);
+
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
